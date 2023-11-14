39b64014545c3579a3267ff1fe0e1c7fb80e63d3
Gurcan-Unsal <gurcanunsal@outlook.com>
2023-11-14 16:57:41 +0300

    initial design

---
+++ src/components/ProfileFinder.js
@@ -0,0 +1,23 @@
+export const ProfileFinder = () => {
+  return (
+    <div>
+      <h2 className="pt-4 text-white fw-bold text-center">
+        Github Profile Finder
+      </h2>
+      <div className="pt-4 container-fluid">
+        <form className="d-flex justify-content-center align-items-center">
+          <div className="form-group">
+            <input
+              type="text"
+              className="dark-input p-2 rounded text-light border-0"
+              placeholder="Github Username"
+            />
+          </div>
+          <button type="submit" className="btn search-btn ms-2">
+            <i className="bi bi-search text-white"></i>
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
